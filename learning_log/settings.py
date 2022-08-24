7044d570a8a8cdf373bf3f55e28eba1deb7cdb58
Ari Jaya <arijaya130@gmail.com>
2022-08-24 11:49:27 +0800

    Added costom 404 and 500 error page.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_6!*rpb^&l&q3g_648v_j&)^l!1oe$0&^oh@0o0z2l!%8_#0kk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
